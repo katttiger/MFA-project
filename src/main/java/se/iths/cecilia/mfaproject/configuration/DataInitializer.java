a910cc5609e268c3debb7985c69a9f4ddb7430ee
katttiger <katttiger@gmail.com>
2026-04-20 14:24:12 +0200

    Initial commit

---
+++ src/main/java/se/iths/cecilia/mfaproject/configuration/DataInitializer.java
@@ -0,0 +1,12 @@
+package se.iths.cecilia.mfaproject.configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
