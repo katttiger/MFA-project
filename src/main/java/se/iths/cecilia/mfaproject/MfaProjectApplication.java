b7ca02f8879eb8676450839a4b07b003fc3a1b1b
katttiger <katttiger@gmail.com>
2026-04-20 14:05:52 +0200

    Initial commit

---
+++ src/main/java/se/iths/cecilia/mfaproject/MfaProjectApplication.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.mfaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfaProjectApplication.class, args);
+    }
+
+}
