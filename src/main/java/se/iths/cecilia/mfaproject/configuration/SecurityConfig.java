6674722a7140b7e0f56a25df11624c528541f32f
katttiger <katttiger@gmail.com>
2026-04-21 13:00:40 +0200

    Edited SecurityConfig to pass test.

--- src/main/java/se/iths/cecilia/mfaproject/configuration/SecurityConfig.java
+++ src/main/java/se/iths/cecilia/mfaproject/configuration/SecurityConfig.java
@@ -1,30 +1,8 @@
 package se.iths.cecilia.mfaproject.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authorization.EnableMultiFactorAuthentication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.FactorGrantedAuthority;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMultiFactorAuthentication(
-        authorities = {
-                FactorGrantedAuthority.PASSWORD_AUTHORITY,
-                FactorGrantedAuthority.OTT_AUTHORITY
-        }
-)
 public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/**").hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .oneTimeTokenLogin(Customizer.withDefaults());
-        return http.build();
-    }
 
 }
