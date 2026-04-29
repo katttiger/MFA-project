2cf09e04d3040ab63485aaf421bfbe9a2c816ed5
katttiger <katttiger@gmail.com>
2026-04-29 10:39:05 +0200

    Added methods for QR-code generation and verification via authenticator app.

--- src/main/java/se/iths/cecilia/mfaproject/controller/MFAController.java
+++ src/main/java/se/iths/cecilia/mfaproject/controller/MFAController.java
@@ -1,21 +1,46 @@
 package se.iths.cecilia.mfaproject.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import se.iths.cecilia.mfaproject.service.MFAService;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.security.Principal;
+
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/mfa")
 public class MFAController {
 
-    @GetMapping("/qr")
-    public String getQRPage() {
-        return "qr";
-    }
+    private final MFAService mfaService;
 
     @GetMapping
-    public String getVerificationPage() {
-        return "verify-2fa";
+    public String getVerificationPage(Model model, Principal principal) {
+        model.addAttribute("username", principal.getName());
+        return "mfa";
     }
 
+    @GetMapping(value = "/qr", produces = "image/png")
+    @ResponseBody
+    public byte[] getQrCode(@RequestParam String username) throws Exception {
+        BufferedImage image = mfaService.generateQrCodeImage(username);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", baos);
+        return baos.toByteArray();
+    }
+
+    @PostMapping("/verify")
+    public String verifyCode(@RequestParam String code, Principal principal, Model model) {
+        boolean isValid = mfaService.verifyCode(principal.getName(), Integer.parseInt(code));
+        if (isValid) {
+            return "redirect:/home";
+        } else {
+            model.addAttribute("error", true);
+            return "mfa";
+        }
+    }
 }
--- src/main/java/se/iths/cecilia/mfaproject/service/MFAService.java
+++ src/main/java/se/iths/cecilia/mfaproject/service/MFAService.java
@@ -1,15 +1,43 @@
 package se.iths.cecilia.mfaproject.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+import java.net.URLEncoder;
 
 @RequiredArgsConstructor
 @Service
 public class MFAService {
     private final AppUserService appUserService;
 
-    //Method: retrieve user
-    //method: generate QR code from secret
+    public BufferedImage generateQrCodeImage(String username) throws Exception {
+        String issuer = "mfaproject".trim();
+        String secret = appUserService.getUser(username).getMfaSecret();
+        username = URLEncoder.encode(username.replace("+", "%20"));
 
+        String otAuthUrl = "otpauth://totp/" + issuer + ":" + username +
+                "?secret=" + secret +
+                "&issuer=" + issuer;
+
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = barcodeWriter.encode(otAuthUrl, BarcodeFormat.QR_CODE, 200, 200);
+
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+    public boolean verifyCode(String username, int code) {
+        String secret = appUserService.getUser(username).getMfaSecret().trim();
+        GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator();
+
+        System.out.println("SECRET: " + secret);
+        System.out.println("CODE: " + code);
+        return googleAuthenticator.authorize(secret, code);
+    }
 
 }
