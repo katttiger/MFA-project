b6e124025ec3c30004d860fba665e3914913b4d3
katttiger <katttiger@gmail.com>
2026-04-29 08:25:56 +0200

    Added MFAService and added AppUserService.getUser.

---
+++ src/main/java/se/iths/cecilia/mfaproject/service/MFAService.java
@@ -0,0 +1,15 @@
+package se.iths.cecilia.mfaproject.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MFAService {
+    private final AppUserService appUserService;
+
+    //Method: retrieve user
+    //method: generate QR code from secret
+
+
+}
